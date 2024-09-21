caf562ab94e4470da0a0bc36f8507e09d801a0c1
이승구 <jeomjang2@gmail.com>
2024-09-21 12:24:17 +0000

    [BOJ] 대소문자 바꾸기 / 브론즈5

---
+++ Lee seunggu/9월 3째주/1차시(9월 21일)/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for(int i=0; i<str.length(); i++)
+        {
+            if (str.charAt(i) < 97)
+            {
+                System.out.printf("%c",str.charAt(i)+32);
+            }
+            else
+            {
+                System.out.printf("%c",str.charAt(i)-32);
+            }
+        }
+    }
+}
